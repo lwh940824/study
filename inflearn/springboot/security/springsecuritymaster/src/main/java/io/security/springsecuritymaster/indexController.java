c210219aa833d3697a6c61f328d7485831c84bcf
whlee <whlee@pluxity.com>
2024-11-07 16:27:02 +0900

    s

--- inflearn/springboot/security/springsecuritymaster/src/main/java/io/security/springsecuritymaster/indexController.java
+++ inflearn/springboot/security/springsecuritymaster/src/main/java/io/security/springsecuritymaster/indexController.java
@@ -7,7 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class indexController {
 
     @GetMapping("/")
-    public  String index() {
+    public String index() {
         return "index";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "loginPage";
     }
 }
