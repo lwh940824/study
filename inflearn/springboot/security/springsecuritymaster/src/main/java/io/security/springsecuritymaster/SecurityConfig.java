2c3d1ade52dc458685e5eaba93a51366208cb56d
WH <lwh940824@gmail.com>
2024-12-02 23:50:43 +0900

    AuthenticationManager

--- inflearn/springboot/security/springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ inflearn/springboot/security/springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -1,31 +1,18 @@
 package io.security.springsecuritymaster;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.NullRequestCache;
-import org.springframework.security.web.savedrequest.RequestCache;
-import org.springframework.security.web.savedrequest.SavedRequest;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import java.io.IOException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -34,17 +21,23 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
-        requestCache.setMatchingRequestParameterName("customParam=y");
+        AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        AuthenticationManager authenticationManager = builder.build();
 
-
-        http.authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/logoutSuccess").permitAll()
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/api/login").permitAll()
                         .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults());
-
+                .authenticationManager(authenticationManager)
+                .addFilterBefore(customAuthenticationFilter(http, authenticationManager), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    public CustomAuthenticationFilter customAuthenticationFilter(HttpSecurity http, AuthenticationManager authenticationManager) {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(http);
+        customAuthenticationFilter.setAuthenticationManager(authenticationManager);
+        return customAuthenticationFilter;
     }
 
     @Bean
--- inflearn/springboot/security/springsecuritymaster/src/main/java/io/security/springsecuritymaster/indexController.java
+++ inflearn/springboot/security/springsecuritymaster/src/main/java/io/security/springsecuritymaster/indexController.java
@@ -1,22 +1,29 @@
 package io.security.springsecuritymaster;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class indexController {
 
+    @Autowired
+    SecurityContextService securityContextService;
+
     @GetMapping("/")
-    public String index(String customParam) {
-        if (customParam != null) {
-            return "customPage";
-        } else {
-            return "index";
-        }
+    public String index() {
+        SecurityContext securityContext = SecurityContextHolder.getContextHolderStrategy().getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        System.out.println("authentication = " + authentication);
+
+        securityContextService.securityContext();
+
+        return authentication.getName();
     }
 
     @GetMapping("/home")
